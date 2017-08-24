5e9955acf8c62a9de1cdb8a2b8352c30b2c67310
Michael Bi <michaelhanbi@gmail.com>
2017-08-24 11:16:14 -0700

    Initial Commit

---
+++ Lift4Life/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lift4Life
+//
+//  Created by Michael Bi on 8/24/17.
+//  Copyright © 2017 Life 4 Life: Strength & Conditioning LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
