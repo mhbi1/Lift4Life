415f268e9c434c812975a4b10f5fc047479cb882
Michael Bi <michaelhanbi@gmail.com>
2017-08-25 21:37:13 -0700

    Implemented side bar. Missing functionality

--- Lift4Life/HomeViewController.swift
+++ Lift4Life/HomeViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    @IBOutlet var swipeGesture: UISwipeGestureRecognizer!
+    var toggle = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.title = "Home"
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.showMenuG(_:)))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.showMenuG(_:)))
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.left
+        self.view.addGestureRecognizer(swipeLeft)
     }
-
+    
+    //Swiping right toggles the menu and swiping left hides it
+    @IBAction func showMenuG(_ sender: UISwipeGestureRecognizer) {
+            revealViewController().revealToggle(animated:toggle)
+            toggle = !toggle
+    }
+    
+    //Clicking on the menu button brings up the menu
+    @IBAction func showMenu(_ sender: UIBarButtonItem) {
+        revealViewController().revealToggle(animated: true)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
