01bb3531ad78ae84638f8a7b95f6fe6c9939d940
Michael Bi <michaelhanbi@gmail.com>
2017-08-25 23:53:25 -0700

    Added appIcon. Added background color and image for team scene Added button images for training programs scene

--- Lift4Life/TPViewController.swift
+++ Lift4Life/TPViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class TPViewController: UIViewController {
 
+    
+    @IBOutlet weak var eliteAthlete: UIButton!
+    @IBOutlet weak var speedster: UIButton!
+    @IBOutlet weak var prodigy: UIButton!
+    @IBOutlet weak var FourthQuarter: UIButton!
+    @IBOutlet weak var loneWolf: UIButton!
+    @IBOutlet weak var dreamTeam: UIButton!
+    
     @IBOutlet weak var menuButton: UIBarButtonItem!
     @IBOutlet var swipeGesture: UISwipeGestureRecognizer!
     var toggle = true
@@ -17,6 +25,7 @@ class TPViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Sets up the swiping gestures to close and open menu
         let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.showMenuG(_:)))
         swipeRight.direction = UISwipeGestureRecognizerDirection.right
         self.view.addGestureRecognizer(swipeRight)
@@ -24,6 +33,18 @@ class TPViewController: UIViewController {
         let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.showMenuG(_:)))
         swipeLeft.direction = UISwipeGestureRecognizerDirection.left
         self.view.addGestureRecognizer(swipeLeft)
+        
+        // Sets the buttons to change when highlighted
+        
+        /*speedster.setBackgroundImage(#imageLiteral(resourceName: "tpButton2") , for: UIControlState.highlighted)
+        prodigy.setBackgroundImage(#imageLiteral(resourceName: "tpButton2") , for: UIControlState.highlighted)
+        FourthQuarter.setBackgroundImage(#imageLiteral(resourceName: "tpButton2") , for: UIControlState.highlighted)
+        loneWolf.setBackgroundImage(#imageLiteral(resourceName: "tpButton2") , for: UIControlState.highlighted)
+        dreamTeam.setBackgroundImage(#imageLiteral(resourceName: "tpButton2") , for: UIControlState.highlighted)*/
+    }
+        
+    @IBAction func eliteAthletePressed(_ sender: UIButton) {
+        eliteAthlete.setBackgroundImage(#imageLiteral(resourceName: "tpButton2") , for: UIControlState.highlighted)
     }
     
     //Swiping right toggles the menu and swiping left hides it
